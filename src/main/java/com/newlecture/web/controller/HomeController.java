2f60d511761bd680f2b6af2f609485e76f7e4405
DESKTOP-7T5S3LD\HJ <hallongjj@naver.com>
2021-06-13 17:39:44 +0900

    text5

--- src/main/java/com/newlecture/web/controller/HomeController.java
+++ src/main/java/com/newlecture/web/controller/HomeController.java
@@ -1,19 +1,39 @@
 package com.newlecture.web.controller;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping("/")
 public class HomeController {
-    
+
+//    @RequestMapping("index")
+//    public void index(HttpServletResponse response) {
+//        
+//        PrintWriter out;
+//        try {
+//            out = response.getWriter();
+//            out.println("Hello Index");
+//        } catch (IOException e) {
+//            // TODO Auto-generated catch block
+//            e.printStackTrace();
+//        }
+//        // return "root.index";
+//    }
+
     @RequestMapping("index")
-    public String index() {
-        
-        return "root.index";
+    @ResponseBody
+    public String index(HttpServletResponse response) {
+
+        return "Hello Index1";
     }
-    
-    
+
 //    @Override
 //    public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
 //
