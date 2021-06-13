59401630f099969efd7de9addcdf5a777e0a71bf
DESKTOP-7T5S3LD\HJ <hallongjj@naver.com>
2021-06-13 12:40:04 +0900

    test4-annotation

--- src/main/java/com/newlecture/web/service/jdbc/JDBCNoticeService.java
+++ src/main/java/com/newlecture/web/service/jdbc/JDBCNoticeService.java
@@ -1,7 +1,6 @@
 package com.newlecture.web.service.jdbc;
 
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -12,9 +11,13 @@ import java.util.List;
 
 import javax.sql.DataSource;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
 import com.newlecture.web.entity.Notice;
 import com.newlecture.web.service.NoticeService;
 
+@Service
 public class JDBCNoticeService implements NoticeService {
 
 //    private String url = "jdbc:oracle:thin:@localhost:1521/xepdb1";
@@ -22,13 +25,9 @@ public class JDBCNoticeService implements NoticeService {
 //    private String uid = "NEWLEC";
 //    private String pwd = "1111";
 
+    @Autowired
     private DataSource dataSource;
     
-    
-    public void setDataSource(DataSource dataSource) {
-        this.dataSource = dataSource;
-    }
-
     public List<Notice> getList(int page, String field, String query) throws ClassNotFoundException, SQLException {
 
         int start = 1 + (page - 1) * 5;
